14e2683fd6a393e7537f83f06a613d77eb994141
TemplaRus <RusTemplaR@yandex.com>
2015-10-31 15:56:48 +0300

    правка инкапсуляции

--- src/trekermanager/Device.java
+++ src/trekermanager/Device.java
@@ -2,23 +2,21 @@ package trekermanager;
 
 public class Device {
 
-     String id = "";
-     int port = 0;
-     boolean connection = false;
-     boolean status = false;
-     private String password = ""; 
-    
-    
+    private String id = "";
+    private int port = 0;
+    private boolean connection = false;
+    private boolean status = false;
+    private String password = "";
+
     public Device() {
-        this("863591026612469", 5601,"1234", false, false);
-    }
-    
-    public Device(String id, int port, String password)
-    {
-       this(id,port,password,false,false);
+        this("863591026612469", 5601, "1234", false, false);
     }
 
-    public Device(String id, int port,String password, boolean connected, boolean status) {
+    public Device(String id, int port, String password) {
+        this(id, port, password, false, false);
+    }
+
+    public Device(String id, int port, String password, boolean connected, boolean status) {
         this.id = id;
         this.port = port;
         this.password = password;
@@ -26,9 +24,6 @@ public class Device {
         this.status = status;
     }
 
-    
-    
-    
     public void setConnection(boolean connection) {
         this.connection = connection;
     }
@@ -60,12 +55,9 @@ public class Device {
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     public void setPort(int port) {
         this.port = port;
     }
-    
-    
-    
-    
+
 }
